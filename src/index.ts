8357fcdd2c67e40cdf080f8b34dbf4df742face1
hein <57179017+heinheinhein@users.noreply.github.com>
2024-04-23 16:08:47 +0200

    Renamed map.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import blessed from "blessed";
 import contrib from "blessed-contrib";
-import map from "./widgets/map.js";
+import peersLocation from "./widgets/peerslocation.js";
 import uploadSpeed from "./widgets/uploadspeed.js";
 
 
@@ -10,19 +10,19 @@ const screen = blessed.screen({
 const gridSize = {
     height: 9,
     width: 12
-}
+};
 const grid = new contrib.grid({ cols: gridSize.width, rows: gridSize.height, screen: screen });
 
 
 // set the grid components
 // grid.set(Y, X, height, width, widget, settings);
-const gridMap = grid.set(0, 0, 5, 9, map.widget, map.settings);
+const gridMap = grid.set(0, 0, 5, 9, peersLocation.widget, peersLocation.settings);
 const gridSparkLine = grid.set(0, 9, 2, 3, uploadSpeed.widget, uploadSpeed.settings);
 
 
 // for the dynamic elements in the grid, we need to update them periodically
-map.startUpdateInterval(gridMap, screen)
-uploadSpeed.startUpdateInterval(gridSparkLine, screen)
+peersLocation.startUpdateInterval(gridMap, screen);
+uploadSpeed.startUpdateInterval(gridSparkLine, screen);
 
 
 screen.key(["escape", "q", "C-c"], function (_ch, _key) {
